b2c2ad69739fd1b9fcd75fd279337365243656fd
Rafael Alberto de Araujo <rafaelalbertola02@gmail.com>
2025-04-11 21:09:07 -0300

    correcao texto

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -6,7 +6,6 @@ function AddTask({ onAddTaskSubmit }) {
 
   return (
     <div className="space-y-4 p-6 bg-slate-500 rounded-md shadow flex flex-col">
-        
       <input
         type="text"
         placeholder="Digite o título da tarefa"
@@ -26,15 +25,15 @@ function AddTask({ onAddTaskSubmit }) {
       <button
         className="bg-slate-700 text-white px-4 py-2 rounded-md font-medium cursor-pointer"
         onClick={() => {
-            if (!title.trim() || !description.trim()) {
-                setTitle("")
-                setDescription("")
-                return alert("Preencha o título e a descrição da tarefa.")
-            }
+          if (!title.trim() || !description.trim()) {
+            setTitle("");
+            setDescription("");
+            return alert("Preencha o título e a descrição da tarefa.");
+          }
 
-            onAddTaskSubmit(title, description)
-            setTitle("")
-            setDescription("")
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
         }}
       >
         Adicionar tarefa
